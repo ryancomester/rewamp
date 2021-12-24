68fffd75265379701ad3512d92b3a71f8c6f7cdb
Ryan Comester <ryancomester@gmail.com>
2021-12-24 20:46:16 +0530

    navbar and basic template

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//dark mode toggle
+
+function darkModeToggle(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+/*function onload_func(){
+    alert("yay");
+}
+
+window.onload = onload_func;*/
