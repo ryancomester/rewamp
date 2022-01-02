05631d8a3e1745f1709913a508fd371187856805
Ryan Comester <ryancomester@gmail.com>
2022-01-02 14:41:29 +0530

    particles and timeline's template
    reference:
    timeline from codepen
    https://codepen.io/TajShireen/pen/JjGvVzg

--- js/pages/myCreativePage.js
+++ js/pages/myCreativePage.js
@@ -4,6 +4,7 @@ Array.prototype.random = function () {
 
 //makes the countdown to 2023
 let countdown_element = document.getElementById("countdown");
+let h2_text = document.getElementById("h2Text");
 
 let countDownDate = new Date("Jan 1, 2023 15:37:35").getTime();
 
@@ -29,6 +30,123 @@ let x = setInterval(function(){
     }
 
     //gives a random color out of the array.
-    colors = ["main-blue", "main-red", "main-green", "main-yellow"];
+    let colors = ["main-blue", "main-red", "main-green", "main-yellow"];
     countdown_element.classList.toggle(colors.random());
+    h2_text.classList.toggle(colors.random());
 }, 1000);
+
+let particleColors = ["#00468a", "#d63031", "#34a752", "#fbbc05"];
+
+//particles.js config
+particlesJS('particles-js',
+{
+    "particles": {
+      "number": {
+        "value": 43,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#5f5454"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3.945738208161363,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#504848",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
